94a1f968a0990ea742f2feddf4483b65f33071ab
Yiheng <development.henryquan@gmail.com>
2022-08-15 00:04:38 +1000

    removed the widget test

--- mqtt/test/widget_test.dart
+++ mqtt/test/widget_test.dart
@@ -5,15 +5,13 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mqtt/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Run the app', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    expect(find.byIcon(Icons.qr_code), findsOneWidget);
   });
 }
